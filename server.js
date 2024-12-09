274c680fb4b52f6c03e3fe74933f434cb54f9e05
deepakgeekmaster <deepakgeekmaster@gmail.com>
2024-12-09 16:45:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,9 +21,17 @@ app.use(cors({
   credentials: true,
 }));
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB Atlas");
+});
 
 
 // Use the property routes
